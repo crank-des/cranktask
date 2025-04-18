80866f02aa80297c96fe634b923643311efbf516
crank-des <cranktasy@gmail.com>
2025-04-18 23:10:35 +0530

    Changes_black2

---
+++ src/Components/ShowDetails.js
@@ -0,0 +1,5 @@
+import { jsx as _jsx } from "react/jsx-runtime";
+const ShowDetails = () => {
+    return _jsx("div", { children: "Show List" });
+};
+export default ShowDetails;
---
+++ src/Components/ShowList.js
@@ -0,0 +1,5 @@
+import { jsx as _jsx } from "react/jsx-runtime";
+const ShowList = () => {
+    return _jsx("div", { children: "Show List" });
+};
+export default ShowList;
